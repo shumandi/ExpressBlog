a9b050a4e16dc21b795556b5c8298acd20e570d7
普通ant <shumandi@live.com>
2016-11-10 11:48:11 +0800

    模版引擎

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 /**
  * Created by mon on 2016/11/10.
  */
+const path = require('path');
 const express = require('express');
 const app = express();
 
 //定义路由中间件
 let indexRouter = require('./routes/index');
 let usersRouter = require('./routes/users');
+
+//设置模版引擎
+app.set('views', path.join(__dirname, 'views')); //设置存储模版的目录
+app.set('view engine', 'ejs');
+
 app.use('/',indexRouter);
 app.use('/users',usersRouter);
 
-app.listen(3000)
+app.listen(3000);
