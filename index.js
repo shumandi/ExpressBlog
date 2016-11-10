a807d681e7e386c99b64242a25fd9123990447d6
普通ant <shumandi@live.com>
2016-11-10 12:18:59 +0800

    功能设计

--- index.js
+++ index.js
@@ -1,25 +1,39 @@
-/**
- * Created by mon on 2016/11/10.
- */
-const path = require('path');
-const express = require('express');
-const app = express();
+var path = require('path');
+var express = require('express');
+var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
+var flash = require('connect-flash');
+var config = require('config-lite');
+var routes = require('./routes');
+var pkg = require('./package');
 
-//定义路由中间件
-let indexRouter = require('./routes/index');
-let usersRouter = require('./routes/users');
+var app = express();
 
-//设置模版引擎
-app.set('views', path.join(__dirname, 'views')); //设置存储模版的目录
+// 设置模板目录
+app.set('views', path.join(__dirname, 'views'));
+// 设置模板引擎为 ejs
 app.set('view engine', 'ejs');
 
-app.use('/',indexRouter);
-app.use('/users',usersRouter);
+// 设置静态文件目录
+app.use(express.static(path.join(__dirname, 'public')));
+// session 中间件
+app.use(session({
+  name: config.session.key,// 设置 cookie 中保存 session id 的字段名称
+  secret: config.session.secret,// 通过设置 secret 来计算 hash 值并放在 cookie 中，使产生的 signedCookie 防篡改
+  cookie: {
+    maxAge: config.session.maxAge// 过期时间，过期后 cookie 中的 session id 自动删除
+  },
+  store: new MongoStore({// 将 session 存储到 mongodb
+    url: config.mongodb// mongodb 地址
+  })
+}));
+// flash 中间价，用来显示通知
+app.use(flash());
 
-//错误处理
-app.use(function(err, req, res, next) {
-  console.error(err.stack);
-  res.status(500).send('Something broke!');
+// 路由
+routes(app);
+
+// 监听端口，启动程序
+app.listen(config.port, function () {
+  console.log(`${pkg.name} listening on port ${config.port}`);
 });
-
-app.listen(3000);
---
+++ routes/signout.js
@@ -0,0 +1,14 @@
+/**
+ * Created by mon on 2016/11/10.
+ */
+var express = require('express');
+var router = express.Router();
+
+var checkLogin = require('../middlewares/check').checkLogin;
+
+// GET /signout 登出
+router.get('/', checkLogin, function(req, res, next) {
+  res.send(req.flash());
+});
+
+module.exports = router;
